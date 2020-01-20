82fac75c9c16745de9c941d04e71198f62d38272
Anastasiia Sharikova <shharikova@gmail.com>
2020-01-20 18:10:25 +0300

    first commit variables

---
+++ 1.1-variables/main.js
@@ -0,0 +1,19 @@
+// 1.1
+let algebra = 4;
+let geography = 4;
+let physics = 5;
+let averageScore;
+averageScore = (algebra + geography + physics)/3;
+console.log(averageScore);
+
+// 1.2
+let name = 'Настя'
+let message = `Привет, мир! Меня зовут ${name}.`
+console.log(message);
+
+// 1.3
+function exerciseVasya(x, y, z) {
+
+    console.log(x * y + 5 * z + x - 1);
+}
+exerciseVasya(10, 2, 5);
