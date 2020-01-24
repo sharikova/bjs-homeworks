077890c22f5b1f4a4f5c22f81a2560c70613557b
Anastasiia Sharikova <shharikova@gmail.com>
2020-01-24 14:06:25 +0300

    first commit extended-syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 function calculateQuadraticEquation(){
     let a = +window.a.value;
@@ -10,21 +10,36 @@ function calculateQuadraticEquation(){
     span.textContent = "х = "+result;
 }
 
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
-}
-
+    function getResult(a,b,c){
+        let discriminant = b**2 - 4 * a * c; 
+        let result = [];
+        if (discriminant < 0) {
+            result = [ ];
+          } else if (discriminant == 0) {
+            result = [-b / ( 2 * a )];
+          } else {
+            result[0] = -b / ( 2 * a ) + Math.sqrt( discriminant ) / ( 2 * a );
+            result[1] = -b / ( 2 * a ) - Math.sqrt( discriminant ) / ( 2 * a );
+          }
+          return result;
+    }
 function calculateAverageRating(){
     let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
     let averageMark = getAverageMark(marks);
     window.averageMark.textContent = averageMark;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
-}
+function getAverageMark(marks) {
+    let marksSum = 0;
+    if (marks.length > 5) {
+        marks.slice(0, 5)
+        console.log("Вы ввели слишком много оценок, оценки после 5ой не будут учитываться");
+    }
+    for (let i = 0; i <= marks.length - 1; i++) {
+        marksSum = marksSum + marks[i];
+    }
+        return marksSum/ marks.length;
+    }
 
 function calculateDrinkTask(){
     let name = window.personName.value;
@@ -34,7 +49,14 @@ function calculateDrinkTask(){
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
+    let year = new Date().getFullYear();
+    let age = year - dateOfBirthday.getFullYear();
+    let resultate;
+    if (age >= 18) {
+        resultate = "Не желаете ли олд-фэшн, " + name + "?";
+    } else {
+        resultate = "Сожалею, " + name + ", но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!";
+    }
+    
+    return resultate;
 }
