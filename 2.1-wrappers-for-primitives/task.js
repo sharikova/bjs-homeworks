cc0a07398347156561a59db725d78905601ce081
Anastasiia Sharikova <shharikova@gmail.com>
2020-02-07 16:51:10 +0300

    first commit wrappers-for-primitives.

--- 2.1-wrappers-for-primitives/task.js
+++ 2.1-wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -10,11 +12,46 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    percent = parseInt(percent);
+    contribution = parseInt(contribution);    
+    amount = parseInt(amount);
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+    if(isNaN(percent) === true){
+        return `Параметр \"${window.percent.placeholder}" содержит неправильное значение ${window.percent.value}.`;
+    }
+
+    if(isNaN(contribution) === true){
+        return `Параметр \"${window.contribution.placeholder}" содержит неправильное значение ${window.contribution.value}.`;
+    }
+
+    if(isNaN(amount) === true){
+        return `Параметр \"${window.amount.placeholder}" содержит неправильное значение ${window.amount.value}.`;
+    }
+    
+    let returnSum = amount - contribution;
+    let monthlyPercent = percent / 100 / 12;
+    date = new Date(date);
+
+    function countMonths(date){
+        let today = new Date (),
+            lookForDate = new Date (date),
+            counter = 0;
+      
+        for(let i = 0; i < 10000 ; i++){
+            if(today.getFullYear() != lookForDate.getFullYear() || today.getMonth() != lookForDate.getMonth()){
+                today.setMonth(today.getMonth() + 1);
+            counter ++;
+          } else {
+            return counter;
+          }
+        }
+    }
+
+    let monthlyPayment = returnSum * (monthlyPercent + monthlyPercent / (((1 + monthlyPercent) ** countMonths(date)) - 1));
+    let totalAmount = monthlyPayment * countMonths(date);
+
+    return totalAmount.toFixed(2)
 }
-
 function sayHello() {
     let name = window.personName.value;
     let greeting = getGreeting(name);
@@ -22,7 +59,13 @@ function sayHello() {
     span.textContent = greeting;
 }
 
+
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+   function getName (name) {
+        if ((name === "") || (name === "undefined") || (!Boolean(name))) { 
+            return "Аноним";
+            } else { 
+                return name };
+   };
+   return (`Привет, мир! Меня зовут ${getName(name)}`);
 }
